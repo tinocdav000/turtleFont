3cd75c9c5adee87d105d316f1ba5052ef58bbe22
tinocdav000 <31281260+tinocdav000@users.noreply.github.com>
2017-09-25 19:22:46 -0500

    Letters through K
    All basic letters drawn through K

--- designFont.py
+++ designFont.py
@@ -1,5 +1,7 @@
 import turtle as t
 t.Screen()
+t.speed(1)
+t.pu()
 
 #Draw a box with dimensions 120x160px with vertical divisions every 40px
 def box():
@@ -62,6 +64,7 @@ def drawA():
         t.fd(40)
         t.lt(90)
 
+
 def drawB():
         #Draws an uppercase B
         #Draw low horizontal stem
@@ -87,16 +90,202 @@ def drawB():
         t.rt(180)
         t.pu()
 
+
 def drawC():
-        #Draws an uppercase C
-        #Go to top corner
-        t.fd(60)
+    #draws uppercase C
+    #go to center right
+    t.fd(60)
+    t.lt(90)
+    t.fd(60)
+    t.rt(180)
+    #Draw c
+    for i in range(360):
+        if i < 45 or i > 315:
+            t.pu()
+        else:
+            t.pd()
+        t.fd(1.048)
+        t.rt(1)
+    #finalize
+    t.fd(60)
+    t.lt(90)
+    t.bk(60)
+
+
+def drawD():
+        #Draws an uppercase D
+        #Go to bottom corner
+        t.bk(60)
+        #Draw stem of D
         t.lt(90)
+        t.pd()
         t.fd(120)
-        #Draw C
-        t.lt(90)
+        #Draw D
+        t.rt(90)
+        t.fd(60)
         for i in range(180):
                 t.fd(1.047)
-                t.lt(1)
+                t.rt(1)
+        t.fd(60)
         #Finalize
-        t.bd(60)
+        t.rt(180)
+        t.fd(60)
+
+
+def drawE():
+    #Draws an uppercase E
+    #Draw bottom stem
+    t.pd()
+    t.fd(60)
+    t.bk(120)
+    #Draw middle stem
+    t.lt(90)
+    t.fd(60)
+    t.rt(90)
+    t.fd(120)
+    t.bk(120)
+    #Draw top stem
+    t.lt(90)
+    t.fd(60)
+    t.rt(90)
+    t.fd(120)
+    #Finalize
+    t.pu()
+    t.bk(60)
+    t.rt(90)
+    t.fd(120)
+    t.lt(90)
+
+
+def drawF():
+    #Draws an uppercase F
+    #Go to initial point
+    t.bk(60)
+    #Draw first stem
+    t.pd()
+    t.lt(90)
+    t.fd(60)
+    t.rt(90)
+    t.fd(120)
+    t.bk(120)
+    #Draw top stem
+    t.lt(90)
+    t.fd(60)
+    t.rt(90)
+    t.fd(120)
+    #Finalize
+    t.pu()
+    t.bk(60)
+    t.rt(90)
+    t.fd(120)
+    t.lt(90)
+
+
+def drawG():
+    #Draws an uppercase G
+    #Go to initial point
+    t.fd(60)
+    t.lt(90)
+    t.fd(120)
+    t.lt(90)
+    #Draw the circle
+    t.pd()
+    t.fd(60)
+    for i in range(270):
+        t.fd(1.048)
+        t.lt(1)
+    #Draw stem
+    t.lt(90)
+    t.fd(60)
+    #Finalize
+    t.pu()
+    t.lt(90)
+    t.fd(60)
+    t.lt(90)
+
+
+def drawH():
+    #Draws an uppercase H
+    #Go to initial point
+    t.bk(60)
+    t.lt(90)
+    #Draw first vertical
+    t.pd()
+    t.fd(120)
+    t.bk(60)
+    #Draw center
+    t.rt(90)
+    t.fd(120)
+    #Draw second vertical
+    t.lt(90)
+    t.fd(60)
+    t.bk(120)
+    #Finalize
+    t.pu()
+    t.rt(90)
+    t.bk(60)
+
+
+def drawI():
+    #You get the pattern
+    #Draw first horizontal
+    t.pd()
+    t.fd(60)
+    t.bk(120)
+    t.fd(60)
+    #Draw center
+    t.rt(90)
+    t.fd(120)
+    #Draw second horizontal
+    t.lt(90)
+    t.fd(60)
+    t.bk(120)
+    #Finalize
+    t.pu()
+    t.fd(60)
+
+
+def drawJ():
+    #go to initial point
+    t.bk(60)
+    t.lt(90)
+    t.fd(120)
+    t.rt(90)
+    #draw the straight parts
+    t.pd()
+    t.fd(120)
+    t.rt(90)
+    t.fd(60)
+    #draw the curve
+    for i in range(180):
+        t.fd(1.048)
+        t.rt(1)
+    #finalize
+    t.pu()
+    t.bk(60)
+    t.rt(90)
+    t.fd(60)
+    
+
+def drawK():
+    x = 26.565
+    d = 134.164
+    #go to initial
+    t.fd(60)
+    #draw diagonals
+    t.pd()
+    t.rt(180+x)
+    t.fd(d)
+    t.rt(180-2*x)
+    t.fd(d)
+    #get to stem
+    t.lt(180)
+    t.fd(d)
+    t.lt(90-x)
+    t.fd(60)
+    t.bk(120)
+    #finalize
+    t.pu()
+    t.fd(120)
+    t.lt(90)
+    t.fd(60)
